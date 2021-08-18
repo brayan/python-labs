a3afd7f1f976e9adbebf54c1e85155b5652d2855
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2021-08-18 13:44:25 -0300

    Finish day 1 from python course

--- udemy-angela-yu-python/main.py
+++ udemy-angela-yu-python/main.py
@@ -1,2 +1,8 @@
 
-print("String with 'single quotes' inside")
+print("String with 'single quotes' " + "inside")
+
+name = input("What's your name?\n")
+print("Hello, " + name + "!")
+
+nameSize = len(name)
+print("String size: " + str(nameSize))
